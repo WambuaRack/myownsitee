1c203209cb5bc03c84d431b297b368030c6cba13
WambuaRack <135209397+WambuaRack@users.noreply.github.com>
2023-07-12 13:56:19 +0300

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,6 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
+
 <script>
 function validateForm() {
   let x = document.forms["myForm"]["fname"].value;
@@ -10,15 +8,3 @@ function validateForm() {
   }
 }
 </script>
-</head>
-<body>
-
-<h2>JavaScript Validation</h2>
-
-<form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
-  Name: <input type="text" name="fname">
-  <input type="submit" value="Submit">
-</form>
-
-</body>
-</html>
